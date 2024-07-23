8a56fdc52053e236be0f8527c2733b25a867cffc
Daiane Cruz <daianercruz12@gmail.com>
2024-07-23 18:04:12 -0300

    feat: estrutura de dados - tuplas

---
+++ Estrurtura de dados/Tuplas/indices_negativos_tuplas.py
@@ -0,0 +1,6 @@
+frutas = (
+    "banana", "laranja", "morango", "uva",
+)
+
+print(frutas[-1])  # uva
+print(frutas[-3])  # laranja
---
+++ Estrurtura de dados/Tuplas/matizes_tuplas.py
@@ -0,0 +1,10 @@
+matriz = (
+    (1, 2 ,3),
+    ("a", "b", "c"),
+    (6, 7, 8)
+)
+
+print(matriz[0]) # (1, 2, 3)
+print(matriz[0][1]) #2
+print(matriz[0][2]) #3
+print(matriz[-2][1]) #b
