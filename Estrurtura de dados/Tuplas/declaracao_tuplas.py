22cc562180fef15160074534434c0cc9dddbaf42
Daiane Cruz <daianercruz12@gmail.com>
2024-07-23 16:54:25 -0300

    feat: estrutura de dados - tuplas

---
+++ Estrurtura de dados/Tuplas/declaracao_tuplas.py
@@ -0,0 +1,15 @@
+frutas = (
+    "banana",
+    "pera",
+    "morango",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
