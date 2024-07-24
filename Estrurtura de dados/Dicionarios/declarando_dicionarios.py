f611f497074df94f567c958ab20f56a4c8bced12
Daiane Cruz <daianercruz12@gmail.com>
2024-07-24 11:47:02 -0300

    feat: estrutura de dados - dicionarios

---
+++ Estrurtura de dados/Dicionarios/declarando_dicionarios.py
@@ -0,0 +1,8 @@
+pessoa = {"nome": "Daiane", "idade": 26}
+print(pessoa)
+
+pessoa = dict(nome="Daiane", idade=26)
+print(pessoa)
+
+pessoa["telefone"] = "6574-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+print(pessoa)
