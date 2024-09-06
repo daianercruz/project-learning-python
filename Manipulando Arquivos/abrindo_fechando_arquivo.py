1640ada77cca51fad3a693e457eabb490a9e8a60
Daiane Cruz <daianercruz12@gmail.com>
2024-09-06 12:19:37 -0300

    feat: introdução de manipulação de arquivos e preparando o ambiente

---
+++ Manipulando Arquivos/abrindo_fechando_arquivo.py
@@ -0,0 +1,11 @@
+# Para ler um arquivo
+file = open('exemplo.txt', 'r')
+
+#para escrever em um arquivo
+file = open('exemplo.txt', 'w')
+
+#para anexar conteúdo a um arquivo existente
+file = open('exemplo, txt', 'a')
+
+#para fechar o arquivo
+file.close()
