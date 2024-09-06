4d693c7f77285eddd0a0b580487b4c14db340f96
Daiane Cruz <daianercruz12@gmail.com>
2024-09-06 11:41:48 -0300

    feat: conceito e exemplo de datetime com horas

--- Date, Datetime e Time/datatime.py
+++ Date, Datetime e Time/datatime.py
@@ -1,6 +1,9 @@
-from datetime import date
+from datetime import date, datetime
 
 data = date(2024, 8, 6)
 print(data)
 
 print(date.today())
+
+data_hora = datetime(2024, 9, 6, 11, 40, 55)
+print(data_hora)
